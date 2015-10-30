c5dc6f26f95f9733f63f4dd164d4187204498270
Mats Vederhus <afr0_at_afr0games.com>
2015-10-30 10:37:19 +0100

    Update Bank.scala

--- part1/src/main/scala/Bank.scala
+++ part1/src/main/scala/Bank.scala
@@ -1,14 +1,15 @@
 
 
-object Bank {
+object Bank 
+{
   
   private var idCounter: Int = 0
   
-  def transaction(from: Account, to: Account, amount: Double): Unit = ??? //Implement
+  def transaction(from: Account, to: Account, amount: Double): Unit = {from.deposit(amount); to.withdraw(amount)}
   
-  def getUniqueId: Int = this.synchronized {
-    idCounter += 1
+  def getUniqueId: Int = 
+  {
+    this.synchronized(idCounter += 1)
     idCounter
-  }
-  
+  } 
 }

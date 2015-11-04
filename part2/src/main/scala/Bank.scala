6008bcab4b149ce1d92836f1cb8d516484d02f13
DeimosA <krvolden@gmail.com>
2015-11-04 14:03:02 +0100

    Global executioncontext

--- part2/src/main/scala/Bank.scala
+++ part2/src/main/scala/Bank.scala
@@ -1,20 +1,28 @@
 import scala.concurrent.forkjoin.ForkJoinPool
+import scala.concurrent.ExecutionContext
 
 class Bank(val allowedAttempts: Integer = 3) {
 
   private val uid = ???
   private val transactionsQueue: TransactionQueue = new TransactionQueue()
   private val processedTransactions: TransactionQueue = new TransactionQueue()
-  private val executorContext = ???
+  private val executorContext = ExecutionContext.global
+  
+  private var idCounter: Int = 0
 
   def addTransactionToQueue(from: Account, to: Account, amount: Double): Unit = {
     transactionsQueue push new Transaction(
       transactionsQueue, processedTransactions, from, to, amount, allowedAttempts)
   }
   
-  def generateAccountId: Int = ???
+  def generateAccountId: Int = this.synchronized {
+    idCounter += 1
+    idCounter
+  }
 
-  private def processTransactions: Unit = ???
+  private def processTransactions: Unit = {
+    executorContext.execute(transactionsQueue.pop)
+  }
 
   def addAccount(initialBalance: Double): Account = {
     new Account(this, initialBalance)
